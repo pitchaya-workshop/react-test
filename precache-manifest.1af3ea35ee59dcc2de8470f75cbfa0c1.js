905c395d3f8517f319231086454de7f9edb1ce77
nop <nopzaaaaa@hotmail.com>
2020-08-17 22:45:33 +0700

    Updates

---
+++ precache-manifest.1af3ea35ee59dcc2de8470f75cbfa0c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61567c07f3a2efa73540624b95d72f84",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "bb4267413a12eab8bce2",
+    "url": "/react-test/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d5d160bd9e4ac53c7092",
+    "url": "/react-test/static/css/main.75a1f4c3.chunk.css"
+  },
+  {
+    "revision": "bb4267413a12eab8bce2",
+    "url": "/react-test/static/js/2.2ea06ac1.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/react-test/static/js/2.2ea06ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d160bd9e4ac53c7092",
+    "url": "/react-test/static/js/main.be4777c4.chunk.js"
+  },
+  {
+    "revision": "bdfd7195e6bfda92f36b",
+    "url": "/react-test/static/js/runtime-main.02144241.js"
+  }
+]);
